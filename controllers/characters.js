06153980de15e3f4cc9d3f9827aeb31bb16343bb
Tom Martin <tom@Toms-iMac.local>
2018-01-02 23:07:21 -0700

    clean up formatting

--- controllers/characters.js
+++ controllers/characters.js
@@ -2,16 +2,18 @@ const knex = require('../db/knex');
 
 module.exports = {
     get_all_characters: (req, res) => {
-        knex.raw(`select c.name, 
-                         c.hair_color, 
-                         c.eye_color, 
-                         g.gender, 
-                         p.name homeworld, 
-                         s.name species 
-                         from characters c 
-                         join genders g on c.gender_id=g.id 
-                         join planets p on c.homeworld_id=p.id 
-                         join species s on c.species_id = s.id`)
-            .then(data => res.send(data.rows));
+        knex.raw(
+            `select 
+                c.name, 
+                c.hair_color, 
+                c.eye_color, 
+                g.gender, 
+                p.name homeworld, 
+                s.name species 
+            from characters c 
+            join genders g on c.gender_id=g.id 
+            join planets p on c.homeworld_id=p.id 
+            join species s on c.species_id = s.id`)
+        .then(data => res.send(data.rows));
     }
 }
